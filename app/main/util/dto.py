812b90e9a08c5c9a0d314f03ec2fe50cbe12b672
Seungwoo Kim <sonicdx886@gmail.com>
2021-07-22 22:27:44 +0900

    2nd commit

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -2,7 +2,7 @@ from flask_restx import Namespace, fields
 
 
 class UserDto:
-    api = Namespace('user', description='user related operations')
+    api = Namespace('user', description='sogang-register related operations')
     user = api.model('user', {
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
@@ -15,3 +15,6 @@ class AuthDto:
         'email': fields.String(required=True, description='The email address'),
         'password': fields.String(required=True, description='The user password '),
     })
+    
+class SearchDto:
+    api = Namespace('search', description='db search related operations')
